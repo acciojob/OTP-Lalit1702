62d864096d90c89b8a870aa2904ec9c53e98fc62
Lalit Patil <147392059+Lalit1702@users.noreply.github.com>
2024-08-18 22:46:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+    const codes = document.querySelectorAll('.code');
+
+    codes.forEach((code, idx) => {
+        code.addEventListener('input', () => {
+            if (code.value.length === 1 && idx < codes.length - 1) {
+                codes[idx + 1].focus();  // Move to the next input
+            }
+        });
+
+        code.addEventListener('keydown', (e) => {
+            if (e.key === 'Backspace') {
+                if (code.value === '') {
+                    if (idx > 0) {
+                        codes[idx - 1].focus();  // Move to the previous input
+                        codes[idx - 1].value = '';  // Clear the previous input
+                    }
+                }
+            }
+        });
+    });
+});
+
